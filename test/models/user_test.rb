41e261f020c5fe850d19091cf16d5ee3691ffc0b
= <=>
2021-05-11 18:36:18 +0530

    Added:user email validation test case

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,9 +12,9 @@ class UserTest < ActiveSupport::TestCase
   end
 
   def test_invalid_user_first_name
-     @user.first_name = ''
-     assert_not @user.valid?
-     assert_equal ["First name can't be blank"], @user.errors.full_messages
+    @user.first_name = ''
+    assert_not @user.valid?
+    assert_equal ["First name can't be blank"], @user.errors.full_messages
   end
 
   def test_invalid_user_last_name
@@ -26,30 +26,46 @@ class UserTest < ActiveSupport::TestCase
   def test_invalid_user_email
     @user.email = ''
     assert_not @user.valid?
-    assert_equal ["Email can't be blank", "Email is invalid"], @user.errors.full_messages
+    assert_equal ["Email can't be blank", 'Email is invalid'], @user.errors.full_messages
   end
 
   def test_invalid_user_first_name_length
-    @user.first_name = "a" * 51
+    @user.first_name = 'a' * 51
     assert_not @user.valid?
-   end
+  end
 
   def test_invalid_user_last_name_length
-     @user.last_name  = "a" * 51
-     assert_not @user.valid?
-   end
- 
-   def test_duplicate_user_email
+    @user.last_name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  def test_duplicate_user_email
     @user.save!
     test_user = @user.dup
-    assert_not test_user.valid? 
-    assert_equal ["Email has already been taken"], test_user.errors.full_messages
-   end
-  
-   def test_invalid_user_email_case
-     test_user = User.new(first_name: 'shaddh', last_name: 'sssdhhd', email: 'SAM@gmail.com',
-                     password: 'mahirali');
-     test_user.save!
-     assert_not @user.valid?
-   end
+    assert_not test_user.valid?
+    assert_equal ['Email has already been taken'], test_user.errors.full_messages
+  end
+
+  def test_lowwer_case_user_email
+    test_user = @user.email.downcase
+    @user.email == test_user
+    assert_not @user.valid?
+    assert_equal ["First name can't be blank"], @user.errors.full_messages
+  end
+
+  def test_validation_user_email
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  def test_invalid_user_email_case
+    test_user = User.new(first_name: 'shadab', last_name: 'ali', email: 'SAM@gmail.com',
+                         password: 'mahirali')
+    test_user.save!
+    assert_not @user.valid?
+  end
 end
