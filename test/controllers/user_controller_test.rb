0b3d43d3ad296db1dca9d72ae3694620d680d5c7
= <=>
2021-05-11 22:09:57 +0530

    Added:circle ci config file change

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,9 +1,9 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'test_helper'
+# require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# class UserControllerTest < ActionDispatch::IntegrationTest
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
