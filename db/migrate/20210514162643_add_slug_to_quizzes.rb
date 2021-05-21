149ac84f361b3846db34f439a28b45f226ab6eff
= <=>
2021-05-21 16:05:08 +0530

    Added: migration for slug and publish

---
+++ db/migrate/20210514162643_add_slug_to_quizzes.rb
@@ -0,0 +1,9 @@
+class AddSlugToQuizzes < ActiveRecord::Migration[6.1]
+  
+  def change
+     change_table :quizzes do |t|
+     t.string :slug, null: false
+     t.boolean :is_publish, default: false    
+     end
+   end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,40 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_518_081_527) do
-  create_table 'answers', force: :cascade do |t|
-    t.string 'value', null: false
-    t.boolean 'is_correct', default: false
-    t.integer 'question_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2021_05_18_081527) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "value", null: false
+    t.boolean "is_correct", default: false
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'title', null: false
-    t.integer 'quiz_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "questions", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "quiz_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'quizzes', force: :cascade do |t|
-    t.string 'title', null: false
-    t.string 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "quizzes", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "slug", null: false
+    t.boolean "is_publish", default: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'first_name', null: false
-    t.string 'last_name', null: false
-    t.string 'email', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'role', default: 0
-    t.string 'password_digest', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0
+    t.string "password_digest", null: false
   end
 
-  add_foreign_key 'answers', 'questions', on_delete: :cascade
-  add_foreign_key 'questions', 'quizzes', on_delete: :cascade
-  add_foreign_key 'quizzes', 'users'
+  add_foreign_key "answers", "questions", on_delete: :cascade
+  add_foreign_key "questions", "quizzes", on_delete: :cascade
+  add_foreign_key "quizzes", "users"
 end
